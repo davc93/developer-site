56aeb78d53417974d762b58a305108f4a848d316
Diego Vergara Casanova <davc93@gmail.com>
2023-10-21 13:28:20 -0300

    fix ts error

--- apps/public-site/src/components/Link/index.ts
+++ apps/public-site/src/components/Link/index.ts
@@ -1,17 +1,16 @@
-import { TypographyColor, TypographySize, TypographyWeight, createTypography } from "../Typography"
+import { TypographyColor, TypographySize, createTypography } from "../Typography"
 
 export interface LinkProps {
     href:string,
     label:string,
-    size?:TypographySize,
-    weight?:TypographyWeight
+    size?:TypographySize
 
 }
 
-export const createLink = ({href,label,size = TypographySize.bodyLarge,weight = TypographyWeight.REGULAR}:LinkProps) => {
+export const createLink = ({href,label,size = TypographySize.bodyLarge}:LinkProps) => {
     const anchor = document.createElement("a")
     anchor.href = href
-    const text = createTypography({label,size,weight,color:TypographyColor.Primary})
+    const text = createTypography({label,size,color:TypographyColor.Primary})
     anchor.append(text)
     return anchor
 }
