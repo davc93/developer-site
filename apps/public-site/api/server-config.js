b4a4001d8578988ff022106c97818e6ae1c37081
Diego Vergara Casanova <davc93@gmail.com>
2023-11-26 16:34:49 -0300

    deletegin env variable en serverless config

--- apps/public-site/api/server-config.js
+++ apps/public-site/api/server-config.js
@@ -1,5 +1,4 @@
 export const config = {
-    postgresDB:process.env.POSTGRES_DB,
     mongoDB:process.env.MONGO_DB,
     smtpEmail:process.env.SMTP_EMAIL,
     smtpPassword:process.env.SMTP_PASSWORD,
