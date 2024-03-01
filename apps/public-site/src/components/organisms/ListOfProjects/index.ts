04a3f10643ae703b4fddf3be61b9f0370b7b732b
Diego Vergara Casanova <davc93@gmail.com>
2024-02-29 22:13:40 -0300

    se modifica componente de list de porjects en pagina de portfolio

--- apps/public-site/src/components/organisms/ListOfProjects/index.ts
+++ apps/public-site/src/components/organisms/ListOfProjects/index.ts
@@ -1,15 +1,13 @@
-
 import { Project } from "../../../models/project.model";
-import {  createProjectCard } from "../../molecules/ProjectCard";
-import "./style.css"
-export function createListOfProjects(projects:Project[]) {
-  const projectList = document.createElement("div")
-  projectList.className = "project-list"  
+import { createProjectCard } from "../../molecules/ProjectCard";
+import "./style.css";
+export function createListOfProjects(projects: Project[]) {
+  const projectList = document.createElement("div");
+  projectList.className = "project-list";
   const projectsElements = projects.map((project) => {
-      
-      return createProjectCard(project)
-    });
+    return createProjectCard(project);
+  });
 
-    projectList.append(...projectsElements)
-    return projectList
-  }
+  projectList.append(...projectsElements);
+  return projectList;
+}
--- apps/public-site/src/routes/portfolio/portfolio.ts
+++ apps/public-site/src/routes/portfolio/portfolio.ts
@@ -1,30 +1,50 @@
 import { createSelect } from "../../components/atoms/Select";
+import { createProjectCard } from "../../components/molecules/ProjectCard";
 import { projectListPortfolio, projectsFilterForm } from "../../nodes";
 import { projectService } from "../../services/project.service";
 import "./style.css";
-import { createListOfProjects } from "../../components/organisms/ListOfProjects";
+import type { Project } from "../../models/project.model";
+
+function createListOfProjects(projects: Project[]) {
+  const projectList = document.createElement("div");
+  projectList.className = "portfolio-project-list";
+  const projectsElements = projects.map((project) => {
+    return createProjectCard(project);
+  });
+
+  projectList.append(...projectsElements);
+  return projectList;
+}
+
+
+
+
 const techs = await projectService.getLabels();
-const options = [{label:"All",value:""},...techs.map((tech) => {
-  return {
-    label: tech.title,
-    value: `${tech.id}`,
-  };
-})] 
+const options = [
+  { label: "All", value: "" },
+  ...techs.map((tech) => {
+    return {
+      label: tech.title,
+      value: `${tech.id}`,
+    };
+  }),
+];
 const selectTechEl = createSelect({ options });
 export const createPortfolioPage = async () => {
+  const projects = await projectService.getProjects();
+  const projectsEl = createListOfProjects(projects);
 
-const projects = await projectService.getProjects();
-const projectsEl = createListOfProjects(projects);
+  
   selectTechEl.addEventListener("change", async (event) => {
     const target = event.target as any;
     const projects = await projectService.getProjects({
       labelId: target?.value,
     });
-    const projectsEl = createListOfProjects(projects)
+    const projectsEl = createListOfProjects(projects);
     while (projectListPortfolio?.firstChild) {
       projectListPortfolio.firstChild.remove();
     }
-    projectListPortfolio?.append(projectsEl)
+    projectListPortfolio?.append(projectsEl);
   });
   projectsFilterForm?.append(selectTechEl);
   projectListPortfolio?.append(projectsEl);
