8b2f4a3c7b653c5c452026a47c99e59382cc13d6
Diego Vergara Casanova <davc93@gmail.com>
2024-04-06 11:41:37 -0300

    avanzando

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "ui-styles/src/variables.css"
+import "ui-styles/src/animations.css"
+
 import "./style.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
