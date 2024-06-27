ad5017e370298158b48c7e92e006aa3d57b38a2c
Diego Vergara Casanova <davc93@gmail.com>
2024-06-27 18:25:54 -0400

    fix ts error

--- apps/public-site/src/routes/home/data/social-media.ts
+++ apps/public-site/src/routes/home/data/social-media.ts
@@ -1,14 +1,14 @@
 import {
   IconGithub,
-  LinkedinIcon
-} from '../../../components/ui/icons/icon-github'
-import { type SocialMedia } from '../../../models/social-media.model'
+  IconLinkedin
+} from '@/components/ui/icons'
+import { type SocialMedia } from '@/models/social-media.model'
 
 export const socialMedia: SocialMedia[] = [
   {
     name: 'Linkedin',
     url: 'https://www.linkedin.com/in/diego-vergara-casanova/',
-    icon: LinkedinIcon({ width: '36px' })
+    icon: IconLinkedin({ width: '36px' })
   },
   {
     name: 'Github',
